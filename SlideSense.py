7988bd2761cbc30726189b34f8ff8b3f766ce004
navaneethchowdary38-lang <navaneethchowdary38@gmail.com>
2026-02-25 09:15:19 +0530

    Update Firebase initialization with structured config
    Refactor Firebase initialization to use structured configuration from secrets.

--- SlideSense.py
+++ SlideSense.py
@@ -18,11 +18,21 @@ from langchain.prompts import ChatPromptTemplate
 st.set_page_config(page_title="SlideSense AI", layout="wide")
 
 # -------------------- FIREBASE INIT --------------------
-import json
-
 if not firebase_admin._apps:
-    firebase_dict = json.loads(json.dumps(st.secrets["firebase"]))
-    cred = credentials.Certificate(firebase_dict)
+    firebase_config = {
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"],
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+    }
+
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
 # -------------------- SESSION --------------------
